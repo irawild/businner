c8970a3e89bb9932a1f2dfac31cd0c949ddc5d5f
irawild <irawildjunior@gmail.com>
2022-11-14 19:46:15 -0300

    postgres connected and working

--- app/sql/database.py
+++ app/sql/database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@businner-db-1/db"
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:businner_password@{}:5432/postgres'.format('db')
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
